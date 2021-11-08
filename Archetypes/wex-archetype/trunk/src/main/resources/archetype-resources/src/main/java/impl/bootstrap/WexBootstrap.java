3d7f6efef1f49fa273389acc98028bad9f0147ec
wincomplm <simonh@wincom-plm.com>
2021-11-08 22:32:47 +0100

    Updated to remove license header

--- Archetypes/wex-archetype/trunk/src/main/resources/archetype-resources/src/main/java/impl/bootstrap/WexBootstrap.java
+++ Archetypes/wex-archetype/trunk/src/main/resources/archetype-resources/src/main/java/impl/bootstrap/WexBootstrap.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ${package}.impl.bootstrap;
 
 import com.wincomplm.wex.kernel.impl.bootstrap.IWexBootstrap;
